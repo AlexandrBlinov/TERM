624adb4a67697e865bac67bd9210fd4a0d968c62
Лапенков Владимир <vlapenkov@ya.ru>
2017-12-08 10:24:33 +0300

    fixed

--- Term.DAL/Resources/ResourceManagerInfo.Designer.cs
+++ Term.DAL/Resources/ResourceManagerInfo.Designer.cs
@@ -19,7 +19,7 @@ namespace Term.DAL.Resources {
     // с помощью такого средства, как ResGen или Visual Studio.
     // Чтобы добавить или удалить член, измените файл .ResX и снова запустите ResGen
     // с параметром /str или перестройте свой проект VS.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class ResourceManagerInfo {
@@ -93,6 +93,15 @@ namespace Term.DAL.Resources {
         public static string InternalPhoneNumber {
             get {
                 return ResourceManager.GetString("InternalPhoneNumber", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Ищет локализованную строку, похожую на Assistants.
+        /// </summary>
+        public static string ManagerAssistants {
+            get {
+                return ResourceManager.GetString("ManagerAssistants", resourceCulture);
             }
         }
         
